3e6fbf4775dc45cbb5befa04011e2f8170e493b0
Adam-Elmi <Adamcade123@gmail.com>
2025-12-31 01:21:02 +0300

    Fix messy ansi code on windows

--- src/runtime/shell.py
+++ src/runtime/shell.py
@@ -114,7 +114,10 @@ class SoplangShell:
                         prompt_text = "\nsoplang> "
                     else:
                         # Colorized prompt for other platforms or with prompt_toolkit
-                        prompt_text = HTML("<ansicyan><b>soplang</b></ansicyan")
+                        if platform.system() == "Windows":
+                            prompt_text = HTML("<ansicyan><b>soplang></b></ansicyan> ")
+                        else:
+                            prompt_text = "\n\033[1;36msoplang>\033[0m "
 
                 # Get user input with the appropriate input method
                 if platform.system() == "Windows" and USE_PROMPT_TOOLKIT:
